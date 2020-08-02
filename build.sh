b330e555dde679f1f2cfeb99eefbc778612581f4
gabriel-wang2008 <gabriel_wang2008@163.com>
2020-08-02 13:33:48 +0800

    s

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo ${IMAGE_NAME}
 echo ${VERSION_ID:-1.0}
 # 获取已经存在的镜像
 ole_image_id=`docker images|grep ${IMAGE_NAME}|grep ${VERSION_ID}|awk '{print $3}'`
-echo ${ole_image_id}
+echo "原来的镜像id ${ole_image_id}"
 # 删掉存在的镜像
 if [[ -n "${ole_image_id}" ]]; then
 	docker rmi -f ${ole_image_id}
@@ -20,10 +20,10 @@ docker build -f dockerfile --build-arg jar_name=${JAR_NAME} -t ${IMAGE_NAME}:${V
 # 获取构建好的镜像的id
 new_image_id=`docker images|grep ${IMAGE_NAME}|grep ${VERSION_ID}|awk '{print $3}'`
 
-echo ${new_image_id}
+echo "构建的镜像id  ${new_image_id}"
 
 # 根据生成的镜像，tag出一个名称空间不同的镜像（腾讯云能识别的镜像）
-#docker tag ${new_image_id} ${IMAGE_NAME}:${VERSION_ID}
+docker tag ${new_image_id} ${IMAGE_NAME}:${VERSION_ID}
 
 # 将镜像仓库能够试别的镜像推送到仓库
-#docker push ${IMAGE_NAME}:${VERSION_ID}
+docker push ${IMAGE_NAME}:${VERSION_ID}
